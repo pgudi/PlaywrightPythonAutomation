5413546ef0c2a18f0273dfd9a335291ae90817a1
pgudi <prabhakargudi@gmail.com>
2026-05-10 22:04:13 +0530

    Login Updated TestScript

--- sampleproject/tests/test_login.py
+++ sampleproject/tests/test_login.py
@@ -1,4 +1,4 @@
-from playwirght.sync_api import Page,expect
+from playwright.sync_api import Page,expect
 
 def test_launch_login(page:Page):
     page.goto("https://sgtestinginstituteapp.onrender.com/")
@@ -8,3 +8,5 @@ def test_launch_login(page:Page):
     page.locator("//input[@name='password']").fill("pgudi")
     page.locator("//button[text()='Sign In']").click()
     page.wait_for_timeout(3000)
+    expect(page).to_have_url("https://sgtestinginstituteapp.onrender.com/home")
+    expect(page).to_have_title("S G Software Testing Institute")
